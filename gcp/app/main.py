6cd3dbfbcb42fc8d673b37dc3aa28f70a3495d7e
Ariel Ortuño Solis <arielg.os@gmail.com>
2026-05-29 17:11:04 -0400

    Service description

--- gcp/app/main.py
+++ gcp/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-app = FastAPI(title="GCP Ambiguity Killer API")
+app = FastAPI(title="GCP Ambiguity Killer", version="1.0.0", description="A service to help clarify GCP concepts and services.")
 
 
 @app.get("/", tags=["root"])
