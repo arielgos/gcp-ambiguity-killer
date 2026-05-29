d731c3fab3efb74f691d4b8b5c84b900c439a44e
Ariel Ortuño Solis <arielg.os@gmail.com>
2026-05-29 17:38:03 -0400

    getting rid of unnecesary endpoints

--- gcp/app/main.py
+++ gcp/app/main.py
@@ -2,12 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI(title="GCP Ambiguity Killer", version="1.0.0", description="A service to help clarify GCP concepts and services.")
 
-
-@app.get("/", tags=["root"])
-def root() -> dict[str, str]:
-    return {"message": "Service is running"}
-
-
 @app.get("/health", tags=["health"])
 def health_check() -> dict[str, str]:
     return {"status": "ok"}
